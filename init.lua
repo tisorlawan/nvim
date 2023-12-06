fa6ba5d1ad3fb6ee95ced3bbb26914c53f782a14
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2023-12-06 19:04:06 +0700

    fix: add gitsigns

--- init.lua
+++ init.lua
@@ -168,9 +168,69 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"romainl/vim-cool",
-	},
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
 
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]g", function()
+						if vim.wo.diff then
+							return "]g"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true, desc = "Next Hunk" })
+
+					map("n", "[g", function()
+						if vim.wo.diff then
+							return "[g"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true, desc = "Prev Hunk" })
+
+					-- Actions
+					map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
+					map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+					map("v", "<leader>gs", function()
+						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "Stage hunk" })
+					map("v", "<leader>gr", function()
+						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "Reset hunk" })
+					map("n", "<leader>gS", gs.stage_buffer, { desc = "Stage buffer" })
+					map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+					map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset buffer" })
+					map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+					map("n", "<leader>gb", function()
+						gs.blame_line({ full = true }, { desc = "Blame line" })
+					end, { desc = "Blame line" })
+					map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle blame line" })
+					map("n", "<leader>gd", gs.diffthis, { desc = "Diff this" })
+					map("n", "<leader>gD", function()
+						gs.diffthis("~")
+					end, { desc = "Diff this ~" })
+					map("n", "<leader>gd", gs.toggle_deleted, { desc = "Toggle deleted" })
+
+					-- Text object
+					map({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
+	{ "romainl/vim-cool" },
 	{
 		"ggandor/leap.nvim",
 		keys = {
@@ -274,10 +334,10 @@ require("lazy").setup({
 		-- optional for icon support
 		-- dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
-			{ "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", silent = true, desc = "[F]iles" },
-			{ "<leader>fh", "<cmd>lua require('fzf-lua').help_tags()<CR>", silent = true, desc = "[H]elp" },
-			{ "<leader>fb", "<cmd>lua require('fzf-lua').buffers()<CR>", silent = true, desc = "[B]buffers" },
-			{ "<leader>fn", "<cmd>lua require('fzf-lua').builtin()<CR>", silent = true, desc = "Built-i[N]" },
+			{ "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", silent = true, desc = "Files" },
+			{ "<leader>fh", "<cmd>lua require('fzf-lua').help_tags()<CR>", silent = true, desc = "Help" },
+			{ "<leader>fb", "<cmd>lua require('fzf-lua').buffers()<CR>", silent = true, desc = "Bbuffers" },
+			{ "<leader>fn", "<cmd>lua require('fzf-lua').builtin()<CR>", silent = true, desc = "Builtin" },
 			{
 				"<leader>fw",
 				"<cmd>lua require('fzf-lua').live_grep_native()<CR>",
@@ -292,6 +352,7 @@ require("lazy").setup({
 				silent = true,
 				desc = "Grep visual",
 			},
+			{ "<leader>fs", "<cmd>lua require('fzf-lua').git_status()<CR>", silent = true, desc = "Git status" },
 		},
 		config = function()
 			-- fix: small delay on closing the window
