1229d33632029cd0440da6d412aafc4e6afa4958
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2023-12-21 13:12:26 +0700

    fix: cmp

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -3,9 +3,12 @@ return {
   event = "InsertEnter",
   config = function()
     require("nvim-autopairs").setup({})
+
     -- If you want insert `(` after select function or method item
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    local cmp = require("cmp")
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    local ok_cmp, cmp = pcall(require, "cmp")
+    if ok_cmp then
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end
   end,
 }
